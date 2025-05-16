9c0d85f1efc18a1308482eccd7b02cf3cdb04327
Reem Khalid <133159514+rsmkhal@users.noreply.github.com>
2025-05-16 15:11:58 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cs396-genai/',
+  base: '/genai-explainer/',
   plugins: [react()],
 })
